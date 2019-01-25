0ac815a1c18b2633964cc94713bcb929cc1b5135
yeonghak seo <inspire12@ncsoft.com>
2019-01-25 21:22:54 +0900

    Init Spring Boot 홈페이지 project
    JPA / Web / devtools  with gradle

---
+++ src/main/java/com/inspire12/homepage/HomepageApplication.java
@@ -0,0 +1,14 @@
+package com.inspire12.homepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomepageApplication.class, args);
+	}
+
+}
+
