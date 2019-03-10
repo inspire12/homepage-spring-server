4cce21b0e1af31ff316efc54d0032115e0919ec8
yeonghakseo <ox4443@naver.com>
2019-03-10 16:18:00 +0900

    Add 서버 관리용 Build 스크립트

--- src/main/java/com/inspire12/homepage/HomepageApplication.java
+++ src/main/java/com/inspire12/homepage/HomepageApplication.java
@@ -1,14 +1,19 @@
 package com.inspire12.homepage;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class HomepageApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HomepageApplication.class, args);
+        SpringApplication application = new SpringApplication(HomepageApplication.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
 	}
+
 
 }
 
---
+++ src/main/java/com/inspire12/homepage/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.inspire12.homepage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String getTest(){
+        return "test";
+    }
+}
