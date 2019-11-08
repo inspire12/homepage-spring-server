a2bed13796938ed8a5bfffac672fe4c92963fc6f
yeonghak seo <inspire12@ncsoft.com>
2019-11-08 20:21:13 +0900

    Update swagger 설정 변경

--- src/main/java/com/inspire12/homepage/config/WebConfig.java
+++ src/main/java/com/inspire12/homepage/config/WebConfig.java
@@ -3,26 +3,28 @@ package com.inspire12.homepage.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
         registry.addResourceHandler(
-                "/webjars/**",
                 "/**",
                 "/img/**",
                 "/css/**",
                 "/js/**")
                 .addResourceLocations(
-                        "classpath:/META-INF/resources/webjars/",
                         "classpath:/static/",
                         "classpath:/static/img/",
                         "classpath:/static/css/",
                         "classpath:/static/js/");
-    }
 
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+    }
 }
