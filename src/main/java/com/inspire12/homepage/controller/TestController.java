15c91483da1c218306fd0cfe9aa1744c257214b2
yeonghak seo <inspire12@ncsoft.com>
2019-11-08 20:22:09 +0900

    Db 관련된 부분 임시 제거

--- src/main/java/com/inspire12/homepage/controller/TestController.java
+++ src/main/java/com/inspire12/homepage/controller/TestController.java
@@ -1,12 +1,14 @@
 package com.inspire12.homepage.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.service.ResponseMessage;
 
 @RestController
 public class TestController {
-
+    @ApiOperation(value = "테스트", response = ResponseMessage.class)
     @GetMapping("/test")
     public String getTest(){
         return "test";
