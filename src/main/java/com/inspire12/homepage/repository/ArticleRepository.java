68a3a9bfef7a6b6d5ccb323a507fb86dc0270fb8
정광진 <dkfwl1771@naver.com>
2019-11-09 23:43:07 +0900

    Add article db 연동

--- src/main/java/com/inspire12/homepage/repository/ArticleRepository.java
+++ src/main/java/com/inspire12/homepage/repository/ArticleRepository.java
@@ -1,19 +1,23 @@
-//package com.inspire12.homepage.repository;
-//
-//import com.inspire12.homepage.model.Article;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.data.repository.query.Param;
-//
-//import javax.persistence.Tuple;
-//import java.util.List;
-//
-//public interface ArticleRepository extends JpaRepository<Article, String> {
-//    List<Article> findAllByBoardId(String boardId);
-//
+package com.inspire12.homepage.repository;
+
+import com.inspire12.homepage.model.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.persistence.Tuple;
+import java.util.List;
+
+public interface ArticleRepository extends JpaRepository<Article, String> {
+
+    List<Article> findAllById(int id);
+
+    // select * from article where board_id = ba
+    List<Article> findAllByBoardId(String boardId);
+
 //    @Query("select a.userId, a.title from Article a where a.userId=:userId")
-//    List<Tuple> selectDistict(@Param("userId") long userId);
+//    List<Tuple> selectDistinct(@Param("userId") long userId);
 //
 //    @Query("select a.userId from Article a where a.userId=:userId")
 //    List<Article> selectAll(@Param("userId") long userId);
-//}
+}
--- src/main/java/com/inspire12/homepage/service/board/ArticleService.java
+++ src/main/java/com/inspire12/homepage/service/board/ArticleService.java
@@ -1,31 +1,27 @@
-//package com.inspire12.homepage.service.board;
-//
-//import com.inspire12.homepage.model.Article;
-//import com.inspire12.homepage.repository.ArticleRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import javax.persistence.Tuple;
-//import javax.persistence.TupleElement;
-//import java.util.HashMap;
-//import java.util.List;
-//
-//@Service
-//public class ArticleService {
-//
-//    @Autowired
-//    private ArticleRepository articleRepository;
-//
+package com.inspire12.homepage.service.board;
+
+import com.inspire12.homepage.model.Article;
+import com.inspire12.homepage.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ArticleService {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
 //    public Article showArticle(String id){
-//
+//        Article article = new Article();
 //
 //        return articleRepository.findById(id).get();
 //    }
-//
-//    public List<Article> showArticleList(){
-//
-//        return articleRepository.findAll();
-//    }
+
+    public List<Article> showArticles(){
+        return articleRepository.findAll();
+    }
 //
 //    public List<Article> showArticleListByBoard(String boardId){
 //        return articleRepository.findAllByBoardId(boardId);
@@ -36,7 +32,7 @@
 //        return articleRepository.save(article);
 ////        return /true;
 //    }
-//
+
 //    public boolean modifyArticle(Article article){
 //        articleRepository.save(article);
 //        return true;
@@ -45,4 +41,4 @@
 //        articleRepository.deleteById(articleId);
 //        return true;
 //    }
-//}
+}
