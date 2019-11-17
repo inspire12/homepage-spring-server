11d946839ad928b4817d4ed8456660ec43273d30
willook@naver.com <willook@naver.com>
2019-11-17 15:11:52 +0900

    Add board 요청시 default parameter 처리

--- src/main/java/com/inspire12/homepage/controller/template/ViewController.java
+++ src/main/java/com/inspire12/homepage/controller/template/ViewController.java
@@ -43,7 +43,7 @@ public class ViewController {
     }
 
     @GetMapping("/board")
-    public String getBoardView(@RequestParam String type, Model model){
+    public String getBoardView(@RequestParam(defaultValue = "all") String type, Model model){
         // board 종류
         model.addAttribute("name", "board");
         return "board";
