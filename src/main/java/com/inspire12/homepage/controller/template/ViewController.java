032d363cdce1434f02f0a8918fb17e55adb90fb0
Jade Yu <50040414+hygoni@users.noreply.github.com>
2019-11-17 15:08:50 +0900

    board에 defaultValue 추가

--- src/main/java/com/inspire12/homepage/controller/template/ViewController.java
+++ src/main/java/com/inspire12/homepage/controller/template/ViewController.java
@@ -1,24 +1,17 @@
 package com.inspire12.homepage.controller.template;
 
-import com.inspire12.homepage.message.ArticleMsg;
-import com.inspire12.homepage.service.board.ArticleService;
-import com.inspire12.homepage.service.outline.HeaderService;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import java.util.List;
 
 @Controller
 @EnableWebMvc
 public class ViewController {
-    @Autowired
-    HeaderService headerService;
-
-    @Autowired
-    ArticleService articleService;
 
     @GetMapping({"/", "/index"})
     public String index(Model model){
@@ -50,17 +43,21 @@ public class ViewController {
         return "gallery";
     }
 
+    @Nullable
     @GetMapping("/board")
-    public String getBoardView(@RequestParam(defaultValue = "all") String type, @RequestParam(defaultValue = "10") int articleCount, Model model){
+    public String getBoardView(@RequestParam(value="type", defaultValue = "all") String type, Model model){
         // board 종류
-        List<ArticleMsg> articles = articleService.showArticleMsgsWithCount(articleCount);
-        model.addAttribute("articles", articles);
         model.addAttribute("name", "board");
         return "board";
     }
+
 
     @GetMapping("/single-blog")
     public String getSingleBlogView(){
         return "single-blog";
     }
+
+
+
+
 }
