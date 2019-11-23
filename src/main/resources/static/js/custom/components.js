ebe3f5dfa2e923f9ee2931acd345c0e70c65fe33
yeonghakseo <ox4443@naver.com>
2019-11-24 00:44:34 +0900

    Add article 에서 breadCrumb 부분 컴포넌트화

--- src/main/resources/static/js/custom/components.js
+++ src/main/resources/static/js/custom/components.js
@@ -3,7 +3,7 @@ const el = (domstring) => {
     return html.body.firstChild;
 };
 
-function createGallaryImgModel (name, tag, delay) {
+function createGallaryImgModel(name, tag, delay) {
     let model = {};
     model.name = name;
     model.tag = tag;
@@ -13,7 +13,7 @@ function createGallaryImgModel (name, tag, delay) {
 
 function createGallaryImg(img, tag, delay) {
     let singleGallayString = '<div class="col-12 col-sm-6 col-lg-3 single_gallery_item '
-        + tag +' mb-30 wow fadeInUp" data-wow-delay="'
+        + tag + ' mb-30 wow fadeInUp" data-wow-delay="'
         + delay + 'ms"><div class="single-portfolio-content"><img src="'
         + img + '" alt=""><div class="hover-content"><a href="'
         + img + '" class="portfolio-img">+</a></div></div></div>'
@@ -22,8 +22,30 @@ function createGallaryImg(img, tag, delay) {
 }
 
 function createFollowAreaImg(imgName) {
-    let followAreaImg =  '<div class="single-instagram-item">\n' + '<img src="img/bg-img/'
-        + imgName +'.jpg" alt="">\n' + '<div class="instagram-hover-content text-center d-flex align-items-center justify-content-center">\n' + '<a href="#">\n' + '<i class="ti-instagram" aria-hidden="true"></i>\n'
-        +'<span>Best of the world,Tasha</span>\n' + '</a>\n' + '</div>\n' + '</div>'
+    let followAreaImg = '<div class="single-instagram-item">\n' + '<img src="img/bg-img/'
+        + imgName + '.jpg" alt="">\n' + '<div class="instagram-hover-content text-center d-flex align-items-center justify-content-center">\n' + '<a href="#">\n' + '<i class="ti-instagram" aria-hidden="true"></i>\n'
+        + '<span>Best of the world,Tasha</span>\n' + '</a>\n' + '</div>\n' + '</div>'
     return el(followAreaImg);
+}
+
+function createBreadcrumbSection(article) {
+    let section = '<section class="breadcrumb-area blog bg-img bg-overlay jarallax" style="background-image: url(img/bg-img/18.jpg);">\n' +
+        '        <div class="container h-100">\n' +
+        '            <div class="row h-100 align-items-center">\n' +
+        '                <div class="col-12">\n' +
+        '                    <div class="breadcrumb-content text-center">\n' +
+        '                        <a href="#" class="btn post-catagory">' + article['category'] + '</a>\n' +
+        '                        <h2 class="page-title">' + article['subject'] + '</h2>\n' +
+        '                        <div class="post-meta">\n' +
+        '                            <a href="#" class="post-author"> By&nbsp;' + article['author']['nickname'] + '</a>\n' +
+        '                            <a href="#" class="post-date">' + article['created_at'] + '</a>\n' +
+        '                            <a href="#" class="post-comments">No Comments</a>\n' +
+        '                        </div>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '            </div>\n' +
+        '        </div>\n' +
+        '    </section>'
+    let sectionElement = el(section);
+    return sectionElement;
 }
