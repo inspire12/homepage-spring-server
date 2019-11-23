d81a681998e00ddd8512ad3466f6672f8523639c
Sinyoung3016 <tasha0415@naver.com>
2019-11-23 19:20:50 +0900

    Update appendGallaryImgModel

--- src/main/resources/static/js/custom/templates/home.js
+++ src/main/resources/static/js/custom/templates/home.js
@@ -14,20 +14,20 @@ function appendFollowAreaImgs(){
         document.getElementById("instagram-feed-area-id").append(createFollowAreaImg(imgs[i]));
     }
 }
-
+function createGallaryImgModel (name, tag, delay) {
+    let model = {};
+    model.name = name;
+    model.tag = tag;
+    model.delay = delay;
+    return model;
+}
 function appendGallaryImgs() {
-    let imgs = ["3", "4", "5", "6", "7", "8", "9", "10", "boyoung_test"];
+
+    let imgs = [createGallaryImgModel("3", "human", 500), createGallaryImgModel("4", "country", 200)
+        , createGallaryImgModel("boyoung_test", "human", 700)];
 
     for (let i = 0; i < imgs.length; i++) {
-        let singleGallayItem;
-        if(i%2 === 0)
-            singleGallayItem = createGallaryImg("/img/bg-img/" + imgs[i] + ".jpg", "human",500);
-        else if(i%3 === 0)
-            singleGallayItem = createGallaryImg("/img/bg-img/" + imgs[i] + ".jpg", "country",500);
-        else if(i%5 === 0)
-            singleGallayItem = createGallaryImg("/img/bg-img/" + imgs[i] + ".jpg", "nature",500);
-        else
-            singleGallayItem = createGallaryImg("/img/bg-img/" + imgs[i] + ".jpg", "video",500);
+        let singleGallayItem = createGallaryImg("/img/bg-img/" + imgs[i].name + ".jpg", imgs[i].tag,imgs[i].delay);
         document.getElementById("alime-portfolio").append(singleGallayItem);
     }
 
