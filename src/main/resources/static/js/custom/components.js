614d9a2dba53bfc4bf1c2d984a6a5df9444571c2
Sinyoung3016 <tasha0415@naver.com>
2019-11-23 17:11:50 +0900

    Add FollowAreaImg, Modity GallaryImg

--- src/main/resources/static/js/custom/components.js
+++ src/main/resources/static/js/custom/components.js
@@ -5,7 +5,18 @@ const el = (domstring) => {
 
 
 function createGallaryImg(img, tag, delay) {
-    let singleGallayString = '<div class="col-12 col-sm-6 col-lg-3 single_gallery_item '+ tag +' mb-30 wow fadeInUp" data-wow-delay="' + delay + 'ms"><div class="single-portfolio-content"><img src="' + img + '" alt=""><div class="hover-content"><a href="' + img + '" class="portfolio-img">+</a></div></div></div>'
-    return singleGallayItem = el(singleGallayString);
+    let singleGallayString = '<div class="col-12 col-sm-6 col-lg-3 single_gallery_item '
+        + tag +' mb-30 wow fadeInUp" data-wow-delay="'
+        + delay + 'ms"><div class="single-portfolio-content"><img src="'
+        + img + '" alt=""><div class="hover-content"><a href="'
+        + img + '" class="portfolio-img">+</a></div></div></div>'
+    return el(singleGallayString);
     // document.getElementById("alime-portfolio").append(singleGallayItem);
+}
+
+function createFollowAreaImg(imgName) {
+    let followAreaImg =  '<div class="single-instagram-item">\n' + '<img src="img/bg-img/'
+        + imgName +'.jpg" alt="">\n' + '<div class="instagram-hover-content text-center d-flex align-items-center justify-content-center">\n' + '<a href="#">\n' + '<i class="ti-instagram" aria-hidden="true"></i>\n'
+        +'<span>Best of the world,Tasha</span>\n' + '</a>\n' + '</div>\n' + '</div>'
+    return el(followAreaImg);
 }
--- src/main/resources/static/js/custom/templates/home.js
+++ src/main/resources/static/js/custom/templates/home.js
@@ -8,11 +8,26 @@ class img {
     }
 }
 
+function appendFollowAreaImgs(){
+    let imgs = ['boyoung_test', "3","18","4","5","6","7"];
+    for (let i=0; i  < imgs.length; i++){
+        document.getElementById("instagram-feed-area-id").append(createFollowAreaImg(imgs[i]));
+    }
+}
+
 function appendGallaryImgs() {
     let imgs = ["3", "4", "5", "6", "7", "8", "9", "10", "boyoung_test"];
 
     for (let i = 0; i < imgs.length; i++) {
-        let singleGallayItem = createGallaryImg("/img/bg-img/" + imgs[i] + ".jpg", "",500);
+        let singleGallayItem;
+        if(i%2 === 0)
+            singleGallayItem = createGallaryImg("/img/bg-img/" + imgs[i] + ".jpg", "human",500);
+        else if(i%3 === 0)
+            singleGallayItem = createGallaryImg("/img/bg-img/" + imgs[i] + ".jpg", "country",500);
+        else if(i%5 === 0)
+            singleGallayItem = createGallaryImg("/img/bg-img/" + imgs[i] + ".jpg", "nature",500);
+        else
+            singleGallayItem = createGallaryImg("/img/bg-img/" + imgs[i] + ".jpg", "video",500);
         document.getElementById("alime-portfolio").append(singleGallayItem);
     }
 
