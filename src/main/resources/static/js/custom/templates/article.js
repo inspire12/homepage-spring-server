adf55e528c851fd355239de4f0c85fcbe506f990
yeonghakseo <ox4443@naver.com>
2019-11-24 12:50:09 +0900

    Add form 입력 폼 컴포넌트화

--- src/main/resources/static/js/custom/templates/article.js
+++ src/main/resources/static/js/custom/templates/article.js
@@ -3,5 +3,23 @@ function main() {
 
 
     document.getElementById("blog-details-text-id")
+    
 }
 
+function createCommentForm () {
+    let commentFormStr = ' <form action="#" method="post">\n' +
+        '                        <div class="row">\n' +
+        '                            <div class="col-12">\n' +
+        '                                <textarea name="message" class="message form-control mb-30" placeholder="Messages"></textarea>\n' +
+        '                            </div>\n' +
+        '                            <div class="col-12">\n' +
+        '                                <button type="submit" class="btn alime-btn btn-2 mt-15">Send Messages</button>\n' +
+        '                            </div>\n' +
+        '                        </div>\n' +
+        '                    </form>';
+    let commentForm = el(commentFormStr);
+
+    let messageTextarea = commentForm.getElementsByClassName("message-name")[0];
+
+    return {"form-section": commentForm, "message": messageTextarea};
+}
