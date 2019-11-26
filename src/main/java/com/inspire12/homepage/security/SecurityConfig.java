d6ad1fac1c3d46b6e7b9e7c6839b659b2c9299f8
yeonghakseo <ox4443@naver.com>
2019-11-27 00:50:10 +0900

    Add security config 적용

--- src/main/java/com/inspire12/homepage/security/SecurityConfig.java
+++ src/main/java/com/inspire12/homepage/security/SecurityConfig.java
@@ -1,17 +1,66 @@
 package com.inspire12.homepage.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalAuthentication
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final DataSource dataSource;
+
+    @Autowired
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username,password, enabled from user where email=?")
+                .authoritiesByUsernameQuery("select username, authority from authorities where username=?")
+                .passwordEncoder(passwordEncoder());
+
+    }
+
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().disable();
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity.httpBasic().disable();
+
+        httpSecurity
+                .authorizeRequests()
+                .antMatchers("/resources/**", "/webjars/**", "/static/**", "/signup", "/").permitAll()
+                .antMatchers("/h2-console/**").access("hasRole('ADMIN') and hasRole('DBA')")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login")
+                .and()
+                .csrf()
+                .ignoringAntMatchers("/h2-console/**")
+                .and()
+                .headers().frameOptions().disable();
     }
 }
