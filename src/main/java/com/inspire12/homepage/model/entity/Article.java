28573b282358153931fe834350281b3a4c9ca159
yeonghakseo <ox4443@naver.com>
2019-12-01 00:25:00 +0900

    Update article model create 함수 정리, id generated 정정

--- src/main/java/com/inspire12/homepage/model/entity/Article.java
+++ src/main/java/com/inspire12/homepage/model/entity/Article.java
@@ -2,7 +2,9 @@ package com.inspire12.homepage.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -15,24 +17,26 @@ import java.time.LocalDateTime;
 @Table(name = "article")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Article {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     int id;
 
     @Nullable
     @Column(name = "no")
-    int grpno;
+    int grpno = 1;
 
     @Nullable
     @Column(name = "grpord")
-    int grpord;
+    int grpord = 0;
 
     @Nullable
     @Column(name = "depth")
-    int depth;
+    int depth = 0;
 
     @Column(name = "subject")
     String subject;
@@ -60,20 +64,28 @@ public class Article {
 
 
     @Column(name = "tags")
-    String tags;
+    String tags = "";
 
     @Column(name = "hit")
-    Integer hit;
+    Integer hit = 0;
 
     @Column(name = "like")
-    Integer like;
+    Integer like = 0;
 
     public static Article createFromRequest(ObjectNode requestBody) {
         Article article = new Article();
         String userId = requestBody.get("user_id").asText();
+        String title = requestBody.get("title").asText();
         String content = requestBody.get("content").asText();
         article.setUsername(userId);
+        article.setSubject(title);
         article.setContent(content);
+        article.setTags("test");
+        article.setBoardId(1);
+        article.setUpdatedAt(LocalDateTime.now());
+        article.setCreatedAt(LocalDateTime.now());
+        article.setHit(0);
+        article.setLike(0);
         return article;
     }
 
