5e721d83eb6df5f77e50dbde232e4380741af623
yeonghakseo <ox4443@naver.com>
2019-12-03 03:07:03 +0900

    답글 구현 전에 글 정렬 id 로 수정

--- src/main/java/com/inspire12/homepage/repository/ArticleRepository.java
+++ src/main/java/com/inspire12/homepage/repository/ArticleRepository.java
@@ -13,14 +13,14 @@ import java.util.List;
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
     List<Article> findTop30ByBoardIdOrderByGrpnoDesc(int boardId);
 
-    @Query(value = "select * from article order by `no` desc limit :articleCount", nativeQuery = true)
+    @Query(value = "select * from article order by `id` desc limit :articleCount", nativeQuery = true)
     List<Article> showArticlesWithArticleCount(@Param(value = "articleCount") int articleCount);
 
 
-    @Query(value = "SELECT a.*, u.nickname FROM homepage.article as a join homepage.user as u on a.user_id=u.id order by `no` limit 30", nativeQuery = true)
+    @Query(value = "SELECT a.*, u.nickname FROM homepage.article as a join homepage.user as u on a.user_id=u.id order by `id` limit 30", nativeQuery = true)
     List<Tuple> selectArticlesWithNickname();
 
-    @Query(value = "select * from `article` order by no desc limit 1,:limit", nativeQuery = true)
+    @Query(value = "select * from `article` order by id desc limit 1,:limit", nativeQuery = true)
     List<Article> selectArticles(@Param("limit") int limit);
 
     @Modifying
