a919d2f36065106afb8367844d23c83f8e1a10af
yeonghakseo <ox4443@naver.com>
2019-12-07 23:47:55 +0900

    Update Our team 부분을 지정해서 보내주게 수정

--- src/main/java/com/inspire12/homepage/controller/template/ViewController.java
+++ src/main/java/com/inspire12/homepage/controller/template/ViewController.java
@@ -5,13 +5,18 @@ import com.inspire12.homepage.model.entity.User;
 import com.inspire12.homepage.repository.UserRepository;
 import com.inspire12.homepage.service.board.ArticleService;
 import com.inspire12.homepage.service.outline.HeaderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -27,52 +32,65 @@ public class ViewController {
     @Autowired
     UserRepository userRepository;
 
+    Logger logger = LoggerFactory.getLogger(ViewController.class);
+
     @GetMapping({"/", "/index"})
-    public String index(Model model){
+    public String index(Model model) {
 
         model.addAttribute("name", "index");
         return "index";
     }
 
     @GetMapping("/signup")
-    public String getSignup(Model model){
+    public String getSignup(Model model) {
         model.addAttribute("name", "signup");
         return "auth/signup";
     }
+
     @GetMapping("/introduce")
-    public String getIntroduceView() {return "introduce";}
+    public String getIntroduceView() {
+        return "introduce";
+    }
 
     @GetMapping("/contact")
-    public String getContactView(){
+    public String getContactView() {
         return "contact";
     }
 
     @GetMapping("/about")
-    public String getAboutView(Model model){
-        List<User> users = userRepository.findAll();
+    public String getAboutView(Model model) {
+        List<User> users = new ArrayList<>();
+        List<String> names = Arrays.asList("inspire12", "hygoni", "Sinyoung3016", "MoonDD99", "wilook");
+        for (String name : names) {
+            try {
+                users.add(userRepository.findById(name).get());
+            } catch (Exception e) {
+                logger.warn("user not found: " + name + e.toString());
+            }
+        }
         Collections.shuffle(users);
         model.addAttribute("users", users);
         return "about";
     }
 
     @GetMapping("/blog")
-    public String getBlogView(){
+    public String getBlogView() {
         return "blog";
     }
 
 
     @GetMapping("/gallery")
-    public String getgalleryView(){
+    public String getgalleryView() {
         return "gallery";
     }
 
     @GetMapping("/board")
-    public String getBoardView(@RequestParam(defaultValue = "all") String type, @RequestParam(defaultValue = "10") int articleCount, Model model){
+    public String getBoardView(@RequestParam(defaultValue = "all") String type, @RequestParam(defaultValue = "10") int articleCount, Model model) {
         // board 종류
         try {
             List<ArticleMsg> articles = articleService.showArticleMsgsWithCount(articleCount);
             model.addAttribute("articles", articles);
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -81,11 +99,14 @@ public class ViewController {
     }
 
     @GetMapping("/article")
-    public String getSingleBlogView(@RequestParam(defaultValue = "1") int id, Model model){
+    public String getSingleBlogView(@RequestParam(defaultValue = "1") int id, Model model) {
         ArticleMsg article;
         try {
             article = articleService.showArticleMsgById(id);
-        }catch (Exception e){
+            if (SecurityContextHolder.getContext().getAuthentication().getPrincipal() != null) {
+
+            }
+        } catch (Exception e) {
             article = new ArticleMsg();
             e.printStackTrace();
         }
@@ -95,7 +116,7 @@ public class ViewController {
     }
 
     @GetMapping("/writing")
-    public String getWriteView(Model model){
+    public String getWriteView(Model model) {
 
         model.addAttribute("name", "write");
         return "writing";
