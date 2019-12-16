abc415cab67d50397ccc4f0037e9498330e3d8c9
yeonghakseo <ox4443@naver.com>
2019-12-16 23:58:02 +0900

    헤더 개편 + Lab 추가

--- src/main/java/com/inspire12/homepage/controller/template/ViewController.java
+++ src/main/java/com/inspire12/homepage/controller/template/ViewController.java
@@ -34,9 +34,23 @@ public class ViewController {
 
     Logger logger = LoggerFactory.getLogger(ViewController.class);
 
+    private List<User> getAdminUsers() {
+        List<User> users = new ArrayList<>();
+        List<String> names = Arrays.asList("inspire12", "hygoni", "Sinyoung3016", "MoonDD99", "wilook");
+        for (String name : names) {
+            try {
+                users.add(userRepository.findById(name).get());
+            } catch (Exception e) {
+                logger.warn("user not found: " + name + e.toString());
+            }
+        }
+        Collections.shuffle(users);
+        return users;
+    }
+
     @GetMapping({"/", "/index"})
     public String index(Model model) {
-
+        model.addAttribute("users", getAdminUsers());
         model.addAttribute("name", "index");
         return "index";
     }
@@ -59,17 +73,7 @@ public class ViewController {
 
     @GetMapping("/about")
     public String getAboutView(Model model) {
-        List<User> users = new ArrayList<>();
-        List<String> names = Arrays.asList("inspire12", "hygoni", "Sinyoung3016", "MoonDD99", "wilook");
-        for (String name : names) {
-            try {
-                users.add(userRepository.findById(name).get());
-            } catch (Exception e) {
-                logger.warn("user not found: " + name + e.toString());
-            }
-        }
-        Collections.shuffle(users);
-        model.addAttribute("users", users);
+        model.addAttribute("users", getAdminUsers());
         return "about";
     }
 
@@ -85,7 +89,7 @@ public class ViewController {
     }
 
     @GetMapping("/board")
-    public String getBoardView(@RequestParam(defaultValue = "all") String type, @RequestParam(defaultValue = "30") int articleCount, @RequestParam(defaultValue = "1") int pageNum ,Model model) {
+    public String getBoardView(@RequestParam(defaultValue = "all") String type, @RequestParam(defaultValue = "30") int articleCount, @RequestParam(defaultValue = "1") int pageNum, Model model) {
         // board 종류
         try {
             List<ArticleMsg> articles = articleService.showArticleMsgsWithCount(type, pageNum, articleCount);
@@ -121,4 +125,25 @@ public class ViewController {
         model.addAttribute("name", "write");
         return "writing";
     }
+
+    @GetMapping("/algorithm")
+    public String getAlgorithmView() {
+        return "algorithm";
+    }
+
+    @GetMapping("/gan")
+    public String getGanView() {
+        return "/gan";
+    }
+
+    @GetMapping("/ai")
+    public String getAiView() {
+        return "ai";
+    }
+
+    @GetMapping("/gif")
+    public String getGifView() {
+        return "gif";
+    }
+
 }
