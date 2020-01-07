9a6c57b5d357e1480646a252b86a5bb2498812cd
yeonghakseo <ox4443@naver.com>
2020-01-08 01:41:19 +0900

    Add Controller 단 Error 메세지 추가

--- src/main/java/com/inspire12/homepage/GlobalExceptionHandler.java
+++ src/main/java/com/inspire12/homepage/GlobalExceptionHandler.java
@@ -3,10 +3,13 @@ package com.inspire12.homepage;
 import com.inspire12.homepage.exception.NotAuthException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -16,14 +19,19 @@ public class GlobalExceptionHandler {
     public String handleNull(Model model, Exception e) {
         System.err.println(e.getClass());
         logger.error("["+e.getClass() + "] " + e.getMessage() + " from user:"+ SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        model.addAttribute("code", 500);
+        model.addAttribute("msg", "에러가 발생했씁니다.");
+        model.addAttribute("timestamp", LocalDateTime.now());
         return "auth/error";
     }
 
     @ExceptionHandler(NotAuthException.class)
-    public String handleNotFound(Exception e) {
+    public String handleNotFound(Model model, Exception e) {
         System.err.println();
-
         logger.error("["+e.getClass() + "] " + e.getMessage() + " from user:"+ SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-        return "error";
+        model.addAttribute("code", 401);
+        model.addAttribute("msg", "에러가 발생했씁니다.");
+        model.addAttribute("timestamp", LocalDateTime.now());
+        return "auth/error";
     }
 }
