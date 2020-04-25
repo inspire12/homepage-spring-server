1485421a4ec7ef1f2e216323c13546baae9c2082
yeonghakseo <ox4443@naver.com>
2020-04-25 19:54:00 +0900

    add tableview

---
+++ src/main/java/com/inspire12/homepage/view_model/TableView.java
@@ -0,0 +1,20 @@
+package com.inspire12.homepage.view_model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
+
+public class TableView {
+    @JsonProperty("table_type")
+    String tableType;
+
+    @JsonProperty("table_title")
+    String tableTitle;
+
+    List<String> columns;
+    List<ObjectNode> rows;
+
+    PageRequest pager;
+}
